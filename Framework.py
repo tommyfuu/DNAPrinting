18384835ae98146e99ae754a85acb9954cc53216
fcl <fcl@WL-203-33.WPA.Claremont.Edu>
2019-10-12 00:18:39 -0700

    stupid change

--- Framework.py
+++ Framework.py
@@ -6,7 +6,7 @@ import os
 
 ##test images
 image1 = 'Screen Shot 2019-07-18 at 2.30.23 PM.png'
-image2 = '/Users/fcl/desktop/DNAPrinting/CROPPED-zhengjianzhao.jpeg'
+image2 = '/Users/fcl/desktop/DNAPrinting/CROPPED-zhe'
 
 
 #find perceived brightnes as a standard
